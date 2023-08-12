6614305507dc459301e4b1131df0cf398e9ed4dc
Räghib Hasan <raghibmidhat@gmail.com>
2023-08-12 21:38:19 +0300

    test(comparator): add test for custom comparator function

--- src/data-structures/utils/comparator.spec.ts
+++ src/data-structures/utils/comparator.spec.ts
@@ -1,14 +1,58 @@
-import Comparator from "./comparator"
+import Comparator from './comparator'
 
-describe("Comparator", () => {
-  it("should compare with default comparator function", () => {
+describe('Comparator', () => {
+  it('should compare with default comparator function', () => {
     const comparator = new Comparator()
 
     expect(comparator.equal(0, 0)).toBe(true)
     expect(comparator.equal(0, 1)).toBe(false)
-    expect(comparator.equal("a", "a")).toBe(true)
+    expect(comparator.equal('a', 'a')).toBe(true)
+
     expect(comparator.lessThan(1, 2)).toBe(true)
     expect(comparator.lessThan(-1, 2)).toBe(true)
-    expect(comparator.lessThan("a", "b")).toBe(true)
+    expect(comparator.lessThan('a', 'b')).toBe(true)
+    expect(comparator.lessThan('a', 'ab')).toBe(true)
+    expect(comparator.lessThan(10, 2)).toBe(false)
+
+    expect(comparator.lessThanOrEqual(10, 2)).toBe(false)
+    expect(comparator.lessThanOrEqual(1, 1)).toBe(true)
+    expect(comparator.lessThanOrEqual(0, 0)).toBe(true)
+
+    expect(comparator.greaterThan(0, 0)).toBe(false)
+    expect(comparator.greaterThan(10, 0)).toBe(true)
+
+    expect(comparator.greaterThanOrEqual(10, 0)).toBe(true)
+    expect(comparator.greaterThanOrEqual(10, 10)).toBe(true)
+    expect(comparator.greaterThanOrEqual(0, 10)).toBe(false)
+  })
+
+  it('should compare with custom comparator function', () => {
+    const customStringComparator = new Comparator((a: string, b: string) => {
+      if (a.length === b.length) {
+        return 0
+      }
+
+      return a.length < b.length ? -1 : 1
+    })
+
+    expect(customStringComparator.equal('a', 'a')).toBe(true)
+    expect(customStringComparator.equal('a', '')).toBe(false)
+
+    expect(customStringComparator.lessThan('b', 'aa')).toBe(true)
+
+    expect(customStringComparator.greaterThanOrEqual('a', 'aa')).toBe(false)
+    expect(customStringComparator.greaterThanOrEqual('aa', 'a')).toBe(true)
+    expect(customStringComparator.greaterThanOrEqual('a', 'a')).toBe(true)
+
+    customStringComparator.reverse()
+
+    expect(customStringComparator.equal('a', 'b')).toBe(true)
+    expect(customStringComparator.equal('a', '')).toBe(false)
+
+    expect(customStringComparator.lessThan('b', 'aa')).toBe(false)
+
+    expect(customStringComparator.greaterThanOrEqual('a', 'aa')).toBe(true)
+    expect(customStringComparator.greaterThanOrEqual('aa', 'a')).toBe(false)
+    expect(customStringComparator.greaterThanOrEqual('a', 'a')).toBe(true)
   })
 })
