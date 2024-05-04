b8233edaa908cc4629f11a7f27c7240aeb3eac5d
Räghib Hasan <raghibmidhat@gmail.com>
2024-05-04 21:32:39 +0300

    add another solution

--- src/algorithms/stack/valid-parentheses/valid-parentheses.spec.ts
+++ src/algorithms/stack/valid-parentheses/valid-parentheses.spec.ts
@@ -1,14 +1,21 @@
-import { isValid } from './valid-parentheses'
+import { isValid, isValid2 } from './valid-parentheses'
 
 describe('Valid parentheses', () => {
   it('is valid', () => {
     expect(isValid('()')).toBe(true)
     expect(isValid('()[]{}')).toBe(true)
     expect(isValid('{[]}')).toBe(true)
+
+    expect(isValid2('()')).toBe(true)
+    expect(isValid2('()[]{}')).toBe(true)
+    expect(isValid2('{[]}')).toBe(true)
   })
 
   it('is invalid', () => {
     expect(isValid('(]')).toBe(false)
     expect(isValid('([)]')).toBe(false)
+
+    expect(isValid2('(]')).toBe(false)
+    expect(isValid2('([)]')).toBe(false)
   })
 })
