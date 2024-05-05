75100aaa8286ef103f46cab9c291c2403e61f4e8
Räghib Hasan <raghibmidhat@gmail.com>
2024-05-05 11:05:14 +0300

    use pytest

--- src/algorithms/arrays/is-monotonic/test_is_monotonic.py
+++ src/algorithms/arrays/is-monotonic/test_is_monotonic.py
@@ -1,27 +1,15 @@
-import unittest
 from is_monotonic import Solution, AnotherSolution
 
 
-class TestIsMonotonic(unittest.TestCase):
-
-    def setUp(self):
-        self.solution = Solution()
-        self.another_solution = AnotherSolution()
-
-    def test_solution_isMonotonic(self):
-        self.assertTrue(self.solution.isMonotonic([1, 2, 2, 3]))
-
-        self.assertTrue(self.solution.isMonotonic([6, 5, 4, 4]))
-
-        self.assertFalse(self.solution.isMonotonic([1, 3, 2]))
-
-    def test_another_solution_isMonotonic(self):
-        self.assertTrue(self.another_solution.isMonotonic([1, 2, 2, 3]))
-
-        self.assertTrue(self.another_solution.isMonotonic([6, 5, 4, 4]))
-
-        self.assertFalse(self.another_solution.isMonotonic([1, 3, 2]))
+def test_solution_isMonotonic():
+    solution = Solution()
+    assert solution.isMonotonic([1, 2, 2, 3])
+    assert solution.isMonotonic([6, 5, 4, 4])
+    assert not solution.isMonotonic([1, 3, 2])
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_another_solution_isMonotonic():
+    another_solution = AnotherSolution()
+    assert another_solution.isMonotonic([1, 2, 2, 3])
+    assert another_solution.isMonotonic([6, 5, 4, 4])
+    assert not another_solution.isMonotonic([1, 3, 2])
